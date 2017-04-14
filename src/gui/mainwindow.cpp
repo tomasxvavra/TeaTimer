tomasxvavra/TeaTimer

commit 4afb1590f3da51fbbd43a737264a38d2eb5d1f32

MainWindow - simplified time conversion

src/gui/mainwindow.cpp
@@ -60,10 +60,8 @@ void MainWindow::updateView(){
 	qint64 elapsedTotal_ms = mElapsedTimer.elapsed();
 	qint64 elapsed_ms = elapsedTotal_ms;
 	qint64 elapsed_s = elapsed_ms/1000;
-	elapsed_ms = elapsed_ms - elapsed_s*1000;
-	qint64 elapsed_min = elapsed_s / 60;
-	elapsed_s = elapsed_s - elapsed_min*60;
-	QTime elapsed(0, elapsed_min, elapsed_s, elapsed_ms);
+
+	auto elapsed = QTime(0,0,0).addMSecs(elapsed_ms).addSecs(elapsed_s);
 	if (!elapsed.isValid()){
 		return;
 	}

commit f3f79fa2b282bb7e48ae719766784c977e7530fe

Fixed bug caused by 4afb1590f3da51fbbd43a737264a38d2eb5d1f32. Time was added twice.

src/gui/mainwindow.cpp
@@ -65,6 +65,7 @@ void MainWindow::updateView(){
 	qint64 elapsedTotal_ms = mElapsedTimer.elapsed();
 	qint64 elapsed_ms = elapsedTotal_ms;
 	qint64 elapsed_s = elapsed_ms/1000;
+	elapsed_ms = elapsed_ms - elapsed_s*1000;
 
 	auto elapsed = QTime(0,0,0).addMSecs(elapsed_ms).addSecs(elapsed_s);
 	if (!elapsed.isValid()){